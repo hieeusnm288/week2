f81b3365f6636c82cac7f31029d72883ef8e6340
Nguyen Hieu <nguyenminhhieu28801@gmail.com>
2024-11-11 17:36:53 +0700

    create-tab-product

---
+++ data.js
@@ -0,0 +1,193 @@
+const productData = {
+  seating: [
+    {
+      img: "./img/product1.png",
+      cate: "SOFA",
+      name: "Odo ottoman",
+      price: 699,
+    },
+    {
+      img: "./img/product2.png",
+      cate: "Dinning Chair",
+      name: "Ready dining chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product3.png",
+      cate: "Dinning Chair",
+      name: "Harbour side dining chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product4.png",
+      cate: "SOFA",
+      name: "Tearoom club chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product5.png",
+      cate: "Dinning Chair",
+      name: "Co dining chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product6.png",
+      cate: "SOFA",
+      name: "Knitting lounge chair",
+      price: 699.0,
+    },
+  ],
+  table: [
+    {
+      img: "./img/product7.png",
+      cate: "Wood Table",
+      name: "Odo Table",
+      price: 699.0,
+    },
+    {
+      img: "./img/product8.png",
+      cate: "Wood Table",
+      name: "Wood Table Good",
+      price: 699.0,
+    },
+    {
+      img: "./img/product9.png",
+      cate: "Dinning Table",
+      name: "Harbour side dining Table",
+      price: 699.0,
+    },
+    {
+      img: "./img/product10.png",
+      cate: "SOFA",
+      name: "Tearoom club chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product11.png",
+      cate: "Dinning Chair",
+      name: "Co dining chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product12.png",
+      cate: "SOFA",
+      name: "Knitting lounge chair",
+      price: 699.0,
+    },
+  ],
+  storage: [
+    {
+      img: "./img/product12.png",
+      cate: "SOFA storage",
+      name: "Odo ottoman",
+      price: 699.0,
+    },
+    {
+      img: "./img/product11.png",
+      cate: "Dinning Chair",
+      name: "Ready dining chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product10.png",
+      cate: "Dinning Chair",
+      name: "Harbour side dining chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product4.png",
+      cate: "SOFA",
+      name: "Tearoom club chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product5.png",
+      cate: "Dinning Chair",
+      name: "Co dining chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product6.png",
+      cate: "SOFA",
+      name: "Knitting lounge chair",
+      price: 699.0,
+    },
+  ],
+  lighting: [
+    {
+      img: "./img/product7.png",
+      cate: "SOFA lighting",
+      name: "Odo ottoman",
+      price: 699.0,
+    },
+    {
+      img: "./img/product11.png",
+      cate: "Dinning Chair",
+      name: "Ready dining chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product8.png",
+      cate: "Dinning Chair",
+      name: "Harbour side dining chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product4.png",
+      cate: "SOFA",
+      name: "Tearoom club chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product5.png",
+      cate: "Dinning Chair",
+      name: "Co dining chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product6.png",
+      cate: "SOFA",
+      name: "Knitting lounge chair",
+      price: 699.0,
+    },
+  ],
+  decor: [
+    {
+      img: "./img/product12.png",
+      cate: "SOFA decor",
+      name: "Odo ottoman",
+      price: 699.0,
+    },
+    {
+      img: "./img/product2.png",
+      cate: "Dinning Chair",
+      name: "Ready dining chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product8.png",
+      cate: "Dinning Chair",
+      name: "Harbour side dining chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product4.png",
+      cate: "SOFA",
+      name: "Tearoom club chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product5.png",
+      cate: "Dinning Chair",
+      name: "Co dining chair",
+      price: 699.0,
+    },
+    {
+      img: "./img/product6.png",
+      cate: "SOFA",
+      name: "Knitting lounge chair",
+      price: 699.0,
+    },
+  ],
+};
+export default productData;
